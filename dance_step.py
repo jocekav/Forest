c79de6c7372aad7d0f7f6cb5bbd9fd8ea62d2db8
jocekav <63812135+jocekav@users.noreply.github.com>
2021-11-08 20:50:39 -0500

    Add files from lab desktop for debugging

--- dance_step.py
+++ dance_step.py
@@ -1,11 +1,8 @@
-from numpy.lib.function_base import iterable
-import pandas as pd
 import numpy as np
-# import breath
 
-class Robot:
-    def __init__(self, init_pos=np.array([0, 0, 0, 90, 0, 0, 0])):
-        self.dance_dict = {}
+class Dance_Step:
+    def __init__(self, name, bpm=60, dance_step_arr=None, dance_step_t_arr=None, csv_path=None):
+        self.name = name
         self.joint1 = []
         self.joint2 = []
         self.joint3 = []
@@ -21,20 +18,13 @@ class Robot:
         self.joint5_t = []
         self.joint6_t = []
         self.joint7_t = []
-        self.dance = []
-        self.dance_t = []
-        self.curr_angle = 90
-        self.init_pos = init_pos
-    
-    def set_neighbors(self, neighbors):
-        self.neighbors = neighbors
-    
-    def get_neighbors(self):
-        return self.neighbors
+        if dance_step_arr:
+            self.add_angles_as_array(dance_step_arr, dance_step_t_arr)
+            self.bpm = bpm
+        elif csv_path:
+            self.add_angles_from_csv(csv_path)
 
-# Add a dance step to the robot's stored list of joint angles and joint angle times
-# Expects size 7 dance step and time steo arrays/lists 
-    def add_dance_step(self, dance_step, dance_step_t):
+    def add_angles_as_array(self, dance_step, dance_step_t):
         self.joint1.append(dance_step[0])
         self.joint2.append(dance_step[1])
         self.joint3.append(dance_step[2])
@@ -50,26 +40,21 @@ class Robot:
         self.joint5_t.append(dance_step_t[4])
         self.joint6_t.append(dance_step_t[5])
         self.joint7_t.append(dance_step_t[6])
-
-# Calculates trajectory of robot's stored dance steps (stored as joint angles and joint angle times)
-# Assigns constant bpm for all steps to create trajectory
-    def calculate_robot_trajectory(self, bpm, final_time=0, time_step=0.001):
+    
+    def add_angles_from_csv(self, csv_path):
+        return
+    
+    def generate_trajectory_csv(self, time_step=0.001):
         joints = np.array([self.joint1[0], self.joint2[0], self.joint3[0], self.joint4[0], self.joint5[0], self.joint6[0], self.joint7[0]])
         joints_t = np.array([self.joint1_t[0], self.joint2_t[0], self.joint3_t[0], self.joint4_t[0], self.joint5_t[0], self.joint6_t[0], self.joint7_t[0]])
 
         # joints = [robot.joint1, robot.joint2, robot.joint3, robot.joint4, robot.joint5, robot.joint6, robot.joint7]
         # joints_t = [robot.joint1_t, robot.joint2_t, robot.joint3_t, robot.joint4_t, robot.joint5_t, robot.joint6_t, robot.joint7_t]
 
-        time_scale = 60/bpm
+        time_scale = 60 / self.bpm
 
         for i in range(len(joints)):
-            if final_time != 0:
-                reset_pos = -1 * sum(joints[i])
-                joints[i].append(reset_pos)
-                joints_t[i].append(final_time)
-
             joints_t[i] = np.around((time_scale * np.array(joints_t[i])), 3)
-
 
         time_sums = np.empty(len(joints_t))
         for i in range(len(joints_t)):
@@ -78,8 +63,8 @@ class Robot:
             else: 
                 time_sums[i] = joints_t[i]
 
-
         time_max = max(time_sums)
+        self.length = time_max
         time_points = int(time_max / time_step) + 1
         #time_step_arr = np.arange(0, time_max, time_step)
 
@@ -145,50 +130,6 @@ class Robot:
                # trajectory[i + robot_loc, :] = trajectory[i + robot_loc, :]
             if i < 3:
                 trajectory[i, :] = -1 * trajectory[i, :]
-        
+                 
         return trajectory
-
-# Generate and combine trajectories for an array of robot objects 
-# (uses a constant bpm across all steps and robots)
-def combine_trajectories(robots, bpm, final_time=0, time_step=0.001):
-    total_traj_df = pd.DataFrame().astype(float)
-    for robot in robots:
-        trajectory = robot.calculate_robot_trajectory(bpm, final_time=0, time_step=0.001)
-        final_position_db = np.transpose(trajectory)
-        final_position = final_position_db[0::6, :]
-        df = pd.DataFrame(final_position).astype(float)
-        total_traj_df = pd.concat([total_traj_df, df], axis=1)
-    return total_traj_df
-
-def init_robots():
-    
-    ROBOT_1 = Robot()
-    ROBOT_2 = Robot()
-    ROBOT_3 = Robot()
-    ROBOT_4 = Robot()
-    ROBOT_5 = Robot()
-    ROBOT_6 = Robot()
-    ROBOT_7 = Robot()
-    ROBOT_8 = Robot()
-    ROBOT_9 = Robot()
-    ROBOT_10 = Robot()
-
-    robots = [ROBOT_1, ROBOT_2, ROBOT_3, ROBOT_4, ROBOT_5, ROBOT_6, ROBOT_7, ROBOT_8, ROBOT_9, ROBOT_10]
-
-    return robots
-
-
-nod = np.array([0,[-2, 2], 0, [10, -10], 0, [70, -70], 0])
-nod_t = np.array([4, [2, 2], 4, [2, 2], 4, [2, 2], 4])
-
-# breaths = breath.breath(4)
-# breaths_t = breath.breath_t(4)
-
-robots = init_robots()
-for robot in robots:
-    # robot.add_dance_step(breaths, breaths_t)
-    robot.add_dance_step(nod, nod_t)
-traj = robots[0].calculate_robot_trajectory(60)
-# traj = combine_trajectories(robots, 60)
-print(traj)
 
